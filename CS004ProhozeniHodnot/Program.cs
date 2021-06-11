4f4090ea8fbc3dc44df3c5b16286d2abf02f2869
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:42:39 +0200

    Initial commit

---
+++ CS004ProhozeniHodnot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CS004ProhozeniHodnot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo1, cislo2, docasne;
+            Console.WriteLine("Zadej první číslo: ");
+            cislo1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej druhé číslo: ");
+            cislo2 = int.Parse(Console.ReadLine());
+            docasne = cislo1;
+            cislo1 = cislo2;
+            cislo2 = docasne;
+            Console.WriteLine("\nPO PROHOZENÍ: ");
+            Console.WriteLine("První číslo: " + cislo1);
+            Console.WriteLine("Druhé číslo: " + cislo2);
+            Console.ReadKey();
+        }
+    }
+}
